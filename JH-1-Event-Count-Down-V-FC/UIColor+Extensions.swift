e1ef028fbfeea70960d9d6a97040e4405ed2096d
AdPiscinam <lab@walimaloui.com>
2022-03-20 21:45:57 +0100

    Add UIColor extension

---
+++ JH-1-Event-Count-Down-V-FC/UIColor+Extensions.swift
@@ -0,0 +1,10 @@
+// UIColor+Extensions.swift
+// JH-1-Event-Count-Down-V-FC
+// Created by Ad Piscinam on 20/03/2022
+// 
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0.31, green: 0.36, blue: 0.99, alpha: 1.00)
+}
