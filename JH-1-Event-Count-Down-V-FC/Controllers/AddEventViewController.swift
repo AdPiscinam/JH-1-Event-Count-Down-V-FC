9facfe607b4b932ec252cec2edc7e6a917f698f2
AdPiscinam <lab@walimaloui.com>
2022-03-21 17:30:28 +0100

    Add AddEventCoordinator, AddEventViewController & AddEventViewModel

---
+++ JH-1-Event-Count-Down-V-FC/Controllers/AddEventViewController.swift
@@ -0,0 +1,20 @@
+// AddEventViewController.swift
+// JH-1-Event-Count-Down-V-FC
+// Created by Ad Piscinam on 21/03/2022
+// 
+
+import UIKit
+
+class AddEventViewController: UIViewController {
+    
+    var viewModel: AddEventViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
---
+++ JH-1-Event-Count-Down-V-FC/Coordinators/AddEventCoordinator.swift
@@ -0,0 +1,18 @@
+// AddEventCoordinator.swift
+// JH-1-Event-Count-Down-V-FC
+// Created by Ad Piscinam on 21/03/2022
+// 
+
+import Foundation
+
+final class AddEventCoordinator: Coordinator {
+   
+    private(set) var childCoordinators: [Coordinator] = []
+    
+    func start() {
+        
+    }
+    
+    
+    
+}
